0783b7d4c079bc4cdcecce5809d4cc616c613389
Philip Castiglione <philipcastiglione@gmail.com>
2015-10-25 20:53:36 +1100

    added mobile nav functionality

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,23 @@
 $(function() {
-  
+  $('#menu').click(showMenu);
+
+  function showMenu() {
+    event.stopPropagation();
+    $(event.target).toggle();
+    $(event.target).nextAll().slideToggle();
+    $('html').on('click', hideMenu);
+  }
+
+  function hideMenu() {
+    $('html').off('click', hideMenu);
+    var $icon = $('#menu');
+    setTimeout(function() {
+      $icon.toggle();
+    }, 450);
+    $icon.nextAll().slideToggle();
+  }
+
+  function word() {
+    console.log('thing');
+  }
 });
